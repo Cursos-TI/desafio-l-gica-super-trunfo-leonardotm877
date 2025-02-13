60e4eba2616bb431df2bdddb9151bf428eabd8e4
leonardotm877 <ltm.leonardomiguel@gmail.com>
2025-02-13 17:25:15 +0000

    Inserindo dados para Jogador 01

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -13,6 +13,45 @@ int main() {
     double densidade1, densidade2;
 
 
+    // Coleta as informações inseridas pelo usuário para o Jogador 01.
+    printf ("----- Jogador 01 ----- \n");
+    printf ("Digite o país: ");
+    scanf ("%s", pais1);
+    printf ("Digite o Código do Estado: ");
+    scanf ("%s", codigoEstado1);
+    printf ("Digite o Código da Cidade: ");
+    scanf ("%s", codigoCidade1);
+    printf ("Digite a população: ");
+    scanf ("%d", &populacao1);
+    printf ("Digite a área (km²): ");
+    scanf ("%f", &area1);
+    printf ("Digite o PIB: ");
+    scanf ("%lf", &pib1);
+    printf ("Digite o n° de Pontos Turísticos: ");
+    scanf ("%d", &turismo1);
+
+
+    // Cálculo do PIB per capita e Densidade Populacional.
+    pibPercapita1 = (populacao1 > 0) ? pib1 / populacao1 : 0;
+    densidade1 = (area1 > 0) ? populacao1 / area1 : 0;
+
+
+    // Exibição dos atributos inseridos pelo usuário para o Jogador 01.
+    printf (" \n Jogador 01 \n");
+    printf ("País: %s \n", pais1);
+    printf ("Código da Carta: %s%s \n", codigoEstado1, codigoCidade1);
+    printf ("População: %d \n", populacao1);
+    printf ("Área: %.2f km \n", area1);
+    printf ("PIB: %.2lf \n", pib1);
+    printf ("Pontos Turísticos: %d \n", turismo1);
+    printf ("PIB per capita: %.2lf \n", pibPercapita1);
+    printf ("Densidade Populacional: %.2lf \n", densidade1);
+
+
+
+
+
+
 
 
     return 0;
